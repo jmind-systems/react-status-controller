7bbd72a0d5f5f75b589ee9071e8ebcebb61d3036
R3tter <uwebtechnology@gmail.com>
2020-06-17 00:05:34 +0300

    Add copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.ts'],
-        alias: {
-            utils: path.resolve(__dirname, 'src/utils/')
-        }
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        library: 'library-name',
-        libraryTarget: 'umd'
-    },
-    optimization: {
-        minimize: true
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      utils: path.resolve(__dirname, 'src/utils/')
     }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'library-name',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './package.json', to: path.resolve(__dirname, 'dist') },
+        { from: './README.md', to: path.resolve(__dirname, 'dist') }
+      ]
+    })
+  ]
 };
