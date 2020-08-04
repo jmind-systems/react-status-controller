33fc247ea4b6cf4c3101fdbab98908d461ed1443
Yarik <uwebtechnology@gmail.com>
2020-08-04 11:14:41 +0300

    set name in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'library-name',
+    library: '@jmind.systems/react-status-controller',
     libraryTarget: 'umd'
   },
   externals: {
